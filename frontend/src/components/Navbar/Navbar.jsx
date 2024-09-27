32779f882828303ac8a222fc194858d23b20b47f
Nagendra-Chowdary <chowdarynagendra006@gmail.com>
2024-09-28 00:58:01 +0530

    Added changes to the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from "react";
 import './Navbar.css'
 
 import logo from '../Assets/Frontend_Assets/logo.png'
 import cart_icon from '../Assets/Frontend_Assets/cart_icon.png'
 const Navbar = () => {
+
+const [menu,setMenu]=useState("shop");
+
   return (
     <div className='navbar' >
       <div className='nav-logo'>
@@ -11,14 +14,15 @@ const Navbar = () => {
         <p>SHOPPER</p>
       </div>
       <ul className="nav-menu">
-        <li>Shop</li>
-        <li>Men</li>
-        <li>Women</li>
-        <li>Kids</li>
+        <li onClick={()=>{setMenu("shop")}}>shop{menu === "shop"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men{menu === "mens"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women{menu === "women"?<hr />:<React.Fragment></React.Fragment>}</li>
+        <li onClick={()=>{setMenu("kids")}}>kids{menu === "kids"?<hr />:<React.Fragment></React.Fragment>}</li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt=''></img>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   )
